8f76ab5d27dfa516a12a927652a788a5a596993f
bgocean <51014032+bgocean@users.noreply.github.com>
2021-05-21 12:04:54 +0300

    branchOne

--- main.js
+++ main.js
@@ -7,24 +7,52 @@ const again = document.getElementById('again');
 const header = document.querySelector('.header');
 
 const game = {
-    ships: [{
-            location: ['26', '36', '46', '56'],
-            hit: ['', '', '', '']
-        },
-        {
-            location: ['11', '12', '13'],
-            hit: ['', '', '']
-        },
-        {
-            location: ['69', '79'],
-            hit: ['', '']
-        },
-        {
-            location: ['32'],
-            hit: ['']
-        },
-    ],
-    shipCount: 4,
+    ships: [],
+    shipCount: 0,
+    optionShip: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1]
+    },
+    generateShip() {
+        for (let i = 0; i < this.optionShip.count.length; i++) {
+            console.log('count:' +
+                this.optionShip.count[i]);
+            for (let j = 0; j < this.optionShip.count[i]; j++) {
+                console.log('size:' +
+                    this.optionShip.size[i]);
+                const size = this.optionShip.size[i];
+                const ship = this.generateOptionShip(size);
+                this.ships.push(ship);
+                this.shipCount++;
+            }
+        }
+    },
+    generateOptionShip(shipSize) {
+        const ship = {
+            hit: [],
+            location: [],
+        };
+
+        const direction = Math.random() < 0.5;
+        // [0 до 0.99999999]
+        // [0 - 0.499999] / [0.5 - 0.99999] 
+        console.log('direction: ', direction);
+        let x, y;
+
+        if (direction) {
+            console.log('горизонтальный');
+            x = Math.floor((Math.random() * 10));
+            y = Math.floor((Math.random() * 10));
+        } else {
+            console.log('вертикальный');
+            x = Math.floor((Math.random() * 10));
+            y = Math.floor((Math.random() * 10));
+        }
+
+
+        return ship;
+    }
+
 };
 
 const play = {
@@ -61,7 +89,7 @@ const show = {
 
 const fire = (event) => {
     const target = event.target;
-    if (target.classList.length > 0 || target.tagName !== 'TD') return;
+    if (target.classList.length > 0 || target.tagName !== 'TD' || !game.shipCount) return;
     show.miss(target);
     play.updateData = 'shoot';
 
@@ -81,7 +109,7 @@ const fire = (event) => {
 
                 game.shipCount -= 1;
 
-                if (game.shipCount < 1) {
+                if (!game.shipCount) {
                     header.textContent = 'Игра Окончена!';
                     header.style.color = 'red';
 
@@ -99,10 +127,16 @@ const fire = (event) => {
 const init = () => {
     enemy.addEventListener('click', fire);
     play.render();
-
+    game.generateShip();
     again.addEventListener('click', () => {
         location.reload();
     });
+    record.addEventListener('dblclick', () => {
+        localStorage.clear();
+        play.record = 0;
+        play.render();
+    });
+    console.log(game.ships);
 
 };
 
